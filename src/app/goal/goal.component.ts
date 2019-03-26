b1765f529cdb5b26e4f970dadc7423bd3bd54225
Dickson <dicksonparlma@gmail.com>
2019-03-26 11:34:59 +0300

    add the providers to the component decorator

--- src/app/goal/goal.component.ts
+++ src/app/goal/goal.component.ts
@@ -5,7 +5,8 @@ import {GoalService} from '../goals/goal.service';
 @Component({
   selector: 'app-goal',
   templateUrl: './goal.component.html',
-  styleUrls: ['./goal.component.css']
+  styleUrls: ['./goal.component.css'],
+  providers:[GoalService],
 })
 export class GoalComponent implements OnInit {
  goals:Goals;
